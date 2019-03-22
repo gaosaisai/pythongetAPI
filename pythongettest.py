ddbd51dc8fd9ffd12ea7ca94c59adb0dd90f5d4c
gaosaisai <gaosaisai1314@163.com>
2019-03-22 17:14:23 +0800

    juheAPI

--- pythongettest.py
+++ pythongettest.py
@@ -3,11 +3,11 @@ import urllib
 import urllib.parse
 import urllib.request
 
-url = "http://reg.haibian.com/login/ajax_login"
+url = "http://v.juhe.cn/laohuangli/d"
 # 定义请求数据，并且对数据进行赋值
 data = {}
-data['loginname'] = 'student08@qq.com'
-data['password'] = '111111'
+data['key'] = '2e42c2614ebe84c6f225ea00b7095242'
+data['data'] = '2018-03-22'
 # 对请求数据进行编码
 data = urllib.parse.urlencode(data)
 # 将数据和URL进行连接
@@ -17,6 +17,6 @@ requestResponse = urllib.request.urlopen(request)
 # 读取服务端返回的数据
 ResponseStr = requestResponse.read()
 # 打印数据
-ResponseStr = ResponseStr.decode("unicode_escape")
+# ResponseStr = ResponseStr.decode("unicode_escape")
 print(requestResponse)
 print (ResponseStr)
